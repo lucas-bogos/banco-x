837ef2a65a9fea013474b5c9306709a8a7d28185
Lucas Bogos <bogoslucas1@gmail.com>
2023-03-16 20:11:49 -0300

    cria página 404

---
+++ src/pages/404.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once __DIR__ . "/pages/partials/head.php";
+
+useHead('Página não encontrada');
+
+require_once __DIR__ . "/pages/partials/header.php";
+
+?>
+
+<p>Nada encontrado</p>
+
+<?php require_once __DIR__ . "/pages/partials/footer.php"; ?>
