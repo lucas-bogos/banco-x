8f70892d6b19a199f5069ecca53a1d790821f483
Lucas Bogos <bogoslucas1@gmail.com>
2023-04-06 20:05:16 -0300

    cria funcionalidade de obter usuário por CPF

---
+++ src/app/GetUserByCpf.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace source\app;
+
+use source\repositories\user\IUserRepository;
+
+class GetUserByCpf {
+  public IUserRepository $repository;
+
+  public function __construct(IUserRepository $repository) {
+    $this->repository = $repository;
+  }
+
+  public function execute(string $cpf): false | array {
+    $user = $this->repository->getUserByCpf($cpf);
+
+    if(!$user) {
+      return false;
+    }
+
+    return $user;
+  }
+}
