5dc7daaaa59836c8a8f5ad338e3c1b582cd1abc9
Lucas Bogos <bogoslucas1@gmail.com>
2023-04-20 20:01:43 -0300

    adiciona funcionalidade de criar usuário no db

--- src/repositories/user/UserRepositoryMySQL.php
+++ src/repositories/user/UserRepositoryMySQL.php
@@ -2,10 +2,23 @@
 
 namespace source\repositories\user;
 
+use source\shared\ConnectionDb;
+
 class UserRepositoryMySQL implements IUserRepository {
   public function createUser(string $name, string $email, string $password, string $cpf): bool
   {
-    return true;
+    $db = ConnectionDb::get(); 
+
+    $query = "INSERT INTO user (name, email, cpf, password) VALUES (:name, :email, :cpf, :password)";
+    
+    $statement = $db->prepare($query);
+
+    $statement->bindValue(":name", $name);
+    $statement->bindValue(":email", $email);
+    $statement->bindValue(":cpf", $cpf);  
+    $statement->bindValue(":password", $password);
+
+    return $statement->execute();
   } 
 
   public function getUserByCpf(string|int $cpf): mixed
